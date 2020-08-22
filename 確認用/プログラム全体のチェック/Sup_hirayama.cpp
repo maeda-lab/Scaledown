792859542f860c2d1459d7f4afcd9f408fedab73
Tomoki-Hirayama <63904821+Tomoki-Hirayama@users.noreply.github.com>
2020-08-22 17:38:38 +0900

    Update Sup_hirayama.cpp

--- 確認用/プログラム全体のチェック/Sup_hirayama.cpp
+++ 確認用/プログラム全体のチェック/Sup_hirayama.cpp
@@ -47,7 +47,7 @@ int main()
     mbed = serial_initialaize(mbed);
     mbed = serial_Config(mbed);
     
-    //���E���W�n���猩�������ʒu�̍��W(�P�ʂ�m)
+    //世界座標系から見た初期位置の座標(単位はm)
     //double master_x = 0.24;
     //double master_y = -0.01;
     //double master_z = -0.095427;
@@ -55,64 +55,63 @@ int main()
     //double master_b = 8.259;
     //double master_c = 88.316;//109.338;// -95.783661;
     
-    //�����ʒu�Ɍ������֐ߊp
+    //初期位置に向かう関節角
     double master_J1 = 0.0;
     double master_J2 = 120.0;
     double master_J3 = 0.0;//109.338;// -95.783661;
 
-    //�����ʒu���W�̌v�Z
+    //初期位置座標の計算
     double master_x = cal_fpx(master_J1, master_J2, master_J3);
     double master_y = cal_fpy(master_J1, master_J2, master_J3);
     double master_z = cal_fpz(master_J1, master_J2, master_J3);
 
-    //���͊p�̕ϊ�
+    //入力角の変換
     double master_a = master_J1;
-    double master_b = (master_J2 - 90.0)* -1;//�@�\�㔽�]���������̂�߂�
+    double master_b = (master_J2 - 90.0)* -1;//機構上反転させたものを戻す
     double master_c = master_J2 + master_J3 - 90.0;
 
-    printf("\n\n\n�����ʒu�͈ȉ��̒ʂ�ł��D\n");
+    printf("\n\n\n初期位置は以下の通りです．\n");
     printf("x,y,z=[%lf,%lf,%lf]\n\n", master_x,master_y, master_z);
-    printf("�����p�͈ȉ��̒ʂ�ł��D\n");
+    printf("初期角は以下の通りです．\n");
     printf("J1,J2,J3=[%lf,%lf,%lf]\n\n", master_J1, master_J2, master_J3);
-    printf("�����p�͈ȉ��̒ʂ�ł��D\n");
+    printf("初期角は以下の通りです．\n");
     printf("a,b,c=[%lf,%lf,%lf]\n\n", master_a, master_b, master_c);
 
     int t = 0;
     while (1)
     {
        printf("==============================================\n");
-       printf("\n�ڕW�l���w�肵�Ă�������\n");
+       printf("\n目標値を指定してください\n");
        posi.x = (_getch()-'0')   * 0.01;
        posi.y = (_getch() - '0') * 0.01;
        posi.z = (_getch() - '0') * 0.01;
 
-       //�����ʒu����̕ψʂ�pos�ɑ��
-       //->�t�^���w�v�Z�ɂ͐��E���W�n���猩���ʒu�𑫂��Z����D
+       //初期位置からの変位をposに代入
+       //->逆運動学計算には世界座標系から見た位置を足し算する．
 
-       //���̕ψʂ̒P�ʂ͉��H
-       printf("�w�肵�������ʒu����̕ψʂ͈ȉ��̒ʂ�ł�\n");
+       //この変位の単位は何？
+       printf("指定した初期位置からの変位は以下の通りです\n");
        printf("x,y,z=%lf,%lf,%lf\n\n", posi.x, posi.y, posi.z);
        double a, b, c;
        double j1, j2, j3;
        double x, y, z;
 
-       //�w�肵���ψʁ�scale�@�{�@�����ʒu
+       //指定した変位＊scale　＋　初期位置
        x = posi.x * scale + master_x;
        y = posi.y * scale + master_y;
        z = posi.z * scale + master_z;
-       printf("���E���W�n�ł̈ʒu�͈ȉ��̒ʂ�ł�\t�������C�X�P�[�����%.4lf�ł�\n",scale);
+       printf("世界座標系での位置は以下の通りです\tただし，スケール比は%.4lfです\n",scale);
        printf("x,y,z=%lf,%lf,%lf\n\n", x, y, z);
 
-       //�t�^���w�̌v�Z
+       //逆運動学の計算
        j1 = cal_J1(x, y , z );
        j3 = cal_J3(x, y , z,j1);
        j2 = cal_J2(x, y , z,j1, j3);
        printf("j1,j2,j3=[%lf,%lf,%lf]\n\n", j1, j2, j3);
        
-       
-
+       //J1J2J3から送信するための角度を計算する
        a = j1-master_a;
-       b = (j2 - 90.0)*(-1) - master_b;//�@�\��̕␳
+       b = (j2 - 90.0)*(-1) - master_b;//機構上の補正
        c = j3 + j2 - 90.0 - master_c;
        
        //a = master_a - j1;
@@ -120,10 +119,10 @@ int main()
        //c = master_c - (j3 + j2);
 
        
-       printf("�ڕW�ʒu�֕ψʂ����邽�߂ɓ��͂���p�x�͎��̒ʂ�ł�\n");
+       printf("目標位置へ変位させるために入力する角度は次の通りです\n");
        printf("a,b,c=[%.3lf , %.3lf , %.3lf]\n\n", a, b, c);
 
-       //�o�b�t�@�[���܂�
+       //バッファーかます
        a = a / 1.0;
        b = b / 0.85;
        c = c / 0.9;
