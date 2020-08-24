801b60346738747cf0064012cdbaf1a649fc243b
Tomoki-Hirayama <63904821+Tomoki-Hirayama@users.noreply.github.com>
2020-08-25 02:18:30 +0900

    Update sweep.cpp

--- sweep/sweep/sweep.cpp
+++ sweep/sweep/sweep.cpp
@@ -1,7 +1,7 @@
-//2020/08/25�@TomokiHirayama
-//�X�C�[�v�Ȑ����쐬����v���O�����ł��D
-//�v�Z���ʂ��ꎞ�I�ɕۑ�����z�񂪃X�^�b�N�T�C�Y�I�[�o�[�̂���
-//�p�ӂł��Ȃ������̂ŁC�v�Z���ʂ����̂܂܏�������ł��܂��D
+//2020/08/25　TomokiHirayama
+//スイープ曲線を作成するプログラムです．
+//計算結果を一時的に保存する配列がスタックサイズオーバーのため
+//用意できなかったので，計算結果をそのまま書き込んでいます．
 
 
 #include<stdio.h>
@@ -10,58 +10,58 @@
 
 #define PI 3.14159265358979
 
-//���g���̒�`[Hz]
-//0.1Hz(=���10�b�Ȃ�A�[���͒ǂ����Ă��邾�낤)
-// 10Hz(=���0.1�b�Ȃ�A�[���͒ǂ����Ȃ����낤)
+//周波数の定義[Hz]
+//0.1Hz(=一周10秒ならアームは追いついているだろう)
+// 10Hz(=一周0.1秒ならアームは追いつかないだろう)
 #define f_min 0.1
 #define f_max 10.0
 
-//���g���̑����̒�`
-//���g����1s������ǂꂾ�����₷��
+//振動数の増分の定義
+//振動数を1sあたりどれだけ増やすか
 float delta_f = 0.25;
 
-//��]���a�̒�`[mm]
-#define r 20.0  //�v�ύX
+//回転半径の定義[mm]
+#define r 20.0  //要変更
 
-//�b���̑����̒�`[s]
-//�T�[�{�̐�����g���ƍ��킹��
+//秒数の増分の定義[s]
+//サーボの制御周波数と合わせる
 //#define delta_t 1/60
 
-//�ʒu��\���\���̂̐ݒ�
+//位置を表す構造体の設定
 struct Pos {
-	float x;//�X�C�[�v�Ȑ�����ړ�����_�̂��鎞��t�ɂ�����x���W
-	float y;//�X�C�[�v�Ȑ�����ړ�����_�̂��鎞��t�ɂ�����y���W
+	float x;//スイープ曲線上を移動する点のある時刻tにおけるx座標
+	float y;//スイープ曲線上を移動する点のある時刻tにおけるy座標
 };
 
 int main(void)
 {
-	Pos pos;//�ꎞ�ۑ�����\����
+	Pos pos;//一時保存する構造体
 	FILE* fp;
 	errno_t error_fp;
-	const char* filename = "sweep.csv";//�t�@�C���̕ۑ���
+	const char* filename = "sweep.csv";//ファイルの保存名
 	printf("%f", delta_f / 60.0);
-	//======file���������݃��[�h�ŃI�[�v��=======
+	//======fileを書き込みモードでオープン=======
 	error_fp = fopen_s(&fp, filename, "w");
 	if (fp==NULL)
 	{
-		printf("�t�@�C����Open�o���܂���!\n");
+		printf("ファイルをOpen出来ません!\n");
 		exit(0);
 	}
 	else
 	{
-		//===========-sweep�Ȑ����쐬����========
-		//"2458"�̓T�[�{�̐�����g������v�Z(40.96s * 60[1/s]=2457.6�C2458�̓_����邱�Ƃ��ł���)
+		//===========-sweep曲線を作成する========
+		//"2458"はサーボの制御周波数から計算(40.96s * 60[1/s]=2457.6個，2458個の点を取ることができる)
 		for (float t = 0.0; t < 2458.0; t++)
 		{
-			//(�P�ʉ~�̔��ar)*(PI)*(����t�ɂ�������g��f)*(����t)
+			//(単位円の半径r)*(PI)*(時刻tにおける振動数f)*(時刻t)
 			pos.x = r * cos(2.0 * PI * (f_min + delta_f / 60.0 * t) * t/60.0);
 			pos.y = r * sin(2.0 * PI * (f_min + delta_f / 60.0 * t) * t/60.0);
 
-			//======�������ݏ������s��=======
+			//======書き込み処理を行う=======
 			fprintf(fp, "%lf,%lf\n", pos.x, pos.y);
 			
 		}
-		printf("�������݊���");
+		printf("書き込み完了");
 		fclose(fp);
 	}
 	return 0;
